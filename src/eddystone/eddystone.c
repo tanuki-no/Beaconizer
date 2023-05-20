8138049757605786672705c9b7b70af17967fd88
Tanuki <vmikhailikov@gmail.com>
2023-05-20 04:23:28 +0300

    It configures, builds and runs OK.

---
+++ src/eddystone/eddystone.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <bluetooth/bluetooth.h>
+#include "btest/btest.h"
+
+int
+main(
+    const int argc,
+    const char* argv[],
+    const char* env[]) {
+
+    return 0;
+}
